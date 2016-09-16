f2b321b0d9d5aa3b4119ad794d6fd80ab1887830
kenichirotanaka <kenichirotanaka@hotmail.com>
2016-09-16 09:39:11 +0900

    SplitViewコントロールをカスタマイズして翻訳カメラの枠組みとなるUIたたき台を作ってみました。

--- TransCamera/js/default.js
+++ TransCamera/js/default.js
@@ -39,73 +39,36 @@
 
     var mySplitView = window.mySplitView = {
         splitView: null,
-        trailClicked: WinJS.UI.eventHandler(function (ev) {
-            var trailId = ev.currentTarget.dataset.trailId;
-            updateUI(allTrails[trailId]);
-        }),
-        homeClicked: WinJS.UI.eventHandler(function (ev) {
-            //The home button has been clicked
+        takePicture: WinJS.UI.eventHandler(function (ev) {
+            // カメラアイコンクリック時のコールバック
+            // カメラを起動してファイルに保存するまでの処理をここから開始します。
+            var msgBox = new Windows.UI.Popups.MessageDialog("ここにカメラアイコン押下時の処理を書きます");
+            msgBox.showAsync()
+
             document.getElementById("app").classList.add("show-home");
-            document.getElementById("app").classList.remove("show-trail");
+            document.getElementById("app").classList.remove("show-settings");
+        }),
+        recognize: WinJS.UI.eventHandler(function (ev) {
+            // 地球アイコンクリック時のコールバック
+            // 画像認識、翻訳といった処理をここから開始します。
+            var msgBox = new Windows.UI.Popups.MessageDialog("地球アイコン押下時の処理を書きます");
+            msgBox.showAsync()
+
+            document.getElementById("app").classList.add("show-home");
+            document.getElementById("app").classList.remove("show-settings");
+        }),
+        settings: WinJS.UI.eventHandler(function (ev) {
+            // 設定アイコンクリック時のコールバック
+            // もし何らかの設定が必要なときはここから開始します。
+            var msgBox = new Windows.UI.Popups.MessageDialog("設定アイコン押下時の処理を書きます");
+            msgBox.showAsync()
+
+            document.getElementById("app").classList.add("show-settings");
+            document.getElementById("app").classList.remove("show-home");
+
         }),
     };
-
-
     //END SPLIT VIEW
-
-
-    function updateUI(trail) {
-
-        //add remove tags
-        document.getElementById("app").classList.add("show-trail");
-        document.getElementById("app").classList.remove("show-home");
-
-        //update title
-        var titleElt = document.body.querySelector(".trail-title");
-        titleElt.textContent = trail.title;
-
-        //update location
-        var locationElt = document.body.querySelector(".trail-location");
-        locationElt.textContent = trail.location;
-
-        //update description
-        var descriptionElt = document.body.querySelector(".trail-description");
-        descriptionElt.textContent = trail.description;
-
-        //update Rating
-        var ratingElt = document.body.querySelector(".rating");
-        ratingElt.winControl.averageRating = trail.averageRating;
-        ratingElt.winControl.userRating = 0;
-    }
-
-    var trailNameToID = {
-        "Snoqualmie Falls Trail": 0,
-        "Foster Island Trail": 1,
-        "Alki Trail": 2
-    }
-
-    var allTrails = [
-        {
-            title: "Snoqualmie Falls Trail", averageRating: 2, location: "Kirkland, WA", preview: "/images/SampleApp/Snoqualmie.jpg", pictureArray: [
-                { type: "item", picture: "/images/SampleApp/Snoqualmie.jpg" },
-                { type: "item", picture: "/images/SampleApp/Snoqualmie2.jpg" }
-
-            ], description: "Snoqualmie Falls is one of Washington state's most popular scenic attractions. More than 1.5 million visitors come to the Falls every year. At the falls, you will find a two-acre park, gift shop, observation deck, the Salish Lodge and the famous 270 foot waterfall."
-        },
-        {
-            title: "Foster Island Trail", averageRating: 4.5, location: "Bellevue, WA", preview: "/images/SampleApp/Foster.jpg", pictureArray: [
-                 { type: "item", picture: "/images/SampleApp/Foster.jpg" },
-                 { type: "item", picture: "/images/SampleApp/Foster2.jpg" }
-
-            ], description: "Foster Island Trail is a 2 mile loop trail located near Seattle, Washington that features a lake and is good for all skill levels. The trail offers a number of activity options and is accessible year-round."
-        },
-        {
-            title: "Alki Trail", averageRating: 1.5, location: "Seattle, WA", preview: "/images/SampleApp/Alki.jpg", pictureArray: [
-                 { type: "item", picture: "/images/SampleApp/Alki.jpg" },
-                 { type: "item", picture: "/images/SampleApp/Alki2.jpg" }
-            ], description: "The Alki Trail rides along the northern and eastern shore of West Seattle along Alki Avenue. Largely riding on a widened sidewalk, separated from traffic by a parking lane and curb, traffic on the trail is separated for bikes and walkers, providing a less stressful experience for walkers and bikers alike. "
-        }
-    ]
 
 
     //processAll
